faef638e0c66d31a5e311d5175a5d231ac584b76
silaslawer <slawer31@gmail.com>
2020-06-09 16:30:59 +0000

    replace about with webview

--- app/src/main/java/com/blm/saytheirnames/fragments/AboutFragment.java
+++ app/src/main/java/com/blm/saytheirnames/fragments/AboutFragment.java
@@ -1,10 +1,15 @@
 package com.blm.saytheirnames.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -20,9 +25,13 @@ public class AboutFragment extends Fragment {
     String twitterLink = "https://twitter.com/SayTheirName_io";
     String slackLink = "https://saytheirnames.slack.com/";
     String repoLink = "https://github.com/Say-Their-Name/";
+    String aboutPageLink = "https://saytheirnames.io/about";
 
     private Button btnTwitter,btnSlack,btnRepo;
 
+    private WebView webView;
+    private ProgressBar progressBar;
+    private WebSettings webSettings;
     private Toolbar toolbar;
 
 
@@ -42,11 +51,34 @@ public class AboutFragment extends Fragment {
         // Inflate the layout for this fragment
         myFragment = inflater.inflate(R.layout.fragment_about, container, false);
 
+        webView = myFragment.findViewById(R.id.webview_content);
+        progressBar = myFragment.findViewById(R.id.progressBar);
+
         toolbar = myFragment.findViewById(R.id.toolbar);
         toolbar.setTitle("");
         ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
 
-        btnRepo = myFragment.findViewById(R.id.btnViewRepo);
+        webView.loadUrl(aboutPageLink);
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                webView.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        /*btnRepo = myFragment.findViewById(R.id.btnViewRepo);
         btnTwitter = myFragment.findViewById(R.id.btnFollowUs);
         btnSlack = myFragment.findViewById(R.id.btnSlackChannel);
 
@@ -56,7 +88,7 @@ public class AboutFragment extends Fragment {
         btnSlack.setOnClickListener(v -> {
         visitPages(slackLink);
         });
-        btnTwitter.setOnClickListener(v -> visitPages(twitterLink));
+        btnTwitter.setOnClickListener(v -> visitPages(twitterLink));*/
 
         return myFragment;
     }
